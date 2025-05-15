06f8b74d1ab0f018587816ade9cef05349d4acfc
Enotik1337 <128303220+Enotik1337@users.noreply.github.com>
2025-05-15 21:51:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const audio = new Audio('sound.mp3');
-audio.loop = true;
-audio.volume = 1.0;
+const sounds = [
+  'sounds/monsterkill.mp3',
+  'sounds/china.mp3',
+];
+
+let audio;
 
 document.body.addEventListener('click', () => {
+  const randomIndex = Math.floor(Math.random() * sounds.length);
+  const soundSrc = sounds[randomIndex];
+
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  audio = new Audio(soundSrc);
+  audio.loop = true;
+  audio.volume = 1.0;
+
   audio.play().catch(err => console.log('Ошибка воспроизведения:', err));
 });
